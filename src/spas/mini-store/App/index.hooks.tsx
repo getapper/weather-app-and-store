d15e00cf8cd8f64d626237815583960022b4522e
gnrlz <loller1324@gmail.com>
2023-11-10 17:50:29 +0100

    moved redux product loading logic in component App

--- src/spas/mini-store/App/index.hooks.tsx
+++ src/spas/mini-store/App/index.hooks.tsx
@@ -2,6 +2,62 @@ import theme from "@/themes";
 import { useCallback, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { actions, selectors } from "@/spas/mini-store/redux-store";
+import { Product } from "@/spas/mini-store/redux-store/slices/products/products.interfaces";
+import tShirtWhite from "@/assets/t-shirt-bianco.jpg";
+import pdpWhite from "@/assets/pdp-white.jpg";
+import tShirtBlue from "@/assets/t-shirt-blue.jpg";
+import pdpBlue from "@/assets/pdp-blue.jpg";
+import tShirtWhiteArchive from "@/assets/t-shirt-fay-archive-bianco.jpg";
+import pdpWhiteArchive from "@/assets/pdp-white-archive.jpg";
+import tShirtBlackArchive from "@/assets/t-shirt-fay-archive-nero.jpg";
+import pdpBlackArchive from "@/assets/pdp-black-archive.jpg";
+
+const fakeData: Product[] = [
+  {
+    id: 0,
+    name: "T-Shirt - Bianco",
+    description:
+      "T-shirt girocollo a manica corta in jersey mélange, arricchita dalla patch logo Fay applicata sul retro. Un raffinato essenziale del guardaroba casual.",
+    sku: "NPMB3471780UCXB001",
+    price: 110,
+    qty: 10,
+    thumbnail: tShirtWhite,
+    pdpImage: pdpWhite,
+  },
+  {
+    id: 1,
+    name: "T-Shirt - Blu",
+    description:
+      "T-shirt girocollo a manica corta in jersey mélange, arricchita dalla patch logo Fay applicata sul retro. Un raffinato essenziale del guardaroba casual.",
+    sku: "NPMB3471780UCXU807",
+    price: 110,
+    qty: 10,
+    thumbnail: tShirtBlue,
+    pdpImage: pdpBlue,
+  },
+  {
+    id: 2,
+    name: "T-Shirt - Fay Archive - Bianco",
+    description:
+      "T-Shirt girocollo a manica corta in jersey di cotone lavato, con stampa personalizzata Fay Archive sul petto.",
+    sku: "MPMB346105LTGGB004",
+    price: 120,
+    qty: 5,
+    thumbnail: tShirtWhiteArchive,
+    pdpImage: pdpWhiteArchive,
+  },
+  {
+    id: 3,
+    name: "T-Shirt - Fay Archive - Nero",
+    description:
+      "T-Shirt girocollo a manica corta in jersey di cotone lavato, con stampa personalizzata Fay Archive sul petto.",
+    sku: "MPMB346105LTGGB999",
+    price: 120,
+    qty: 5,
+    thumbnail: tShirtBlackArchive,
+    pdpImage: pdpBlackArchive,
+  },
+];
 
 const useAppHooks = () => {
   const dispatch = useDispatch();
@@ -12,6 +68,10 @@ const useAppHooks = () => {
   useEffect(() => {
     dispatch(actions.appStartup());
   }, [dispatch]);
+
+  useEffect(() => {
+    dispatch(actions.loadProducts(fakeData));
+  }, [dispatch, fakeData]);
 
   const handleClose = useCallback(() => {
     dispatch(actions.closeFeedback());
