c5f17ea2df9dc5cb3041e335ff7204f3fa5dd7b6
gnrlz <loller1324@gmail.com>
2023-11-10 17:56:14 +0100

    minor fix

--- src/spas/mini-store/scenes/StoreHomepage/index.tsx
+++ src/spas/mini-store/scenes/StoreHomepage/index.tsx
@@ -1,14 +1,13 @@
 import React, { memo } from "react";
 import { useStoreHomepage } from "./index.hooks";
-import { Container, Grid, Typography, Box, Button } from "@mui/material";
+import { Container, Grid, Typography, Box } from "@mui/material";
 import { ProductCard } from "@/components/ProductCard";
-import CartIcon from "@mui/icons-material/LocalMallOutlined";
-import { Link } from "react-router-dom";
+import { CartButton } from "@/components/CartButton";
 
 type StoreHomepageProps = {};
 
 export const StoreHomepage = memo(({}: StoreHomepageProps) => {
-  const { productList, cartItemsNumber } = useStoreHomepage();
+  const { productList } = useStoreHomepage();
 
   return (
     <Container
@@ -24,24 +23,7 @@ export const StoreHomepage = memo(({}: StoreHomepageProps) => {
         }}
       >
         <Typography variant="h2">SHOP</Typography>
-        <Link to="/checkout">
-          <Button
-            variant={"contained"}
-            startIcon={<CartIcon sx={{ width: 24, height: 24 }} />}
-            sx={{
-              color: "white",
-              background: "none",
-              width: 80,
-              height: 80,
-              borderRadius: "50%",
-              border: "1px solid #d2dde2",
-              boxShadow: "8px 8px 12px -4px rgba(204,204,204,0.1)",
-              "&:hover": { background: "#1a2631" },
-            }}
-          >
-            {cartItemsNumber}
-          </Button>
-        </Link>
+        <CartButton />
       </Box>
       <Grid container columns={10}>
         {productList.map(({ id, name, price, thumbnail }) => (
