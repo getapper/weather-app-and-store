a4a15d95d7c808118a0ffd37ca4df87be575837c
gnrlz <loller1324@gmail.com>
2023-11-10 17:58:20 +0100

    Implemented Checkout scene

--- src/spas/mini-store/scenes/CheckoutPage/index.hooks.tsx
+++ src/spas/mini-store/scenes/CheckoutPage/index.hooks.tsx
@@ -1,3 +1,106 @@
+import { useDispatch, useSelector } from "react-redux";
+import { actions, selectors } from "@/spas/mini-store/redux-store";
+import { useCallback, useMemo, useState } from "react";
+import { Order } from "@/spas/mini-store/redux-store/slices/products/products.interfaces";
+import { useNavigate } from "react-router-dom";
+
 export const useCheckoutPage = () => {
-  return {};
+  const [isLoading, setIsLoading] = useState(false);
+  const [name, setName] = useState("");
+  const [shippingAddress, setShippingAddress] = useState("");
+  const [billingAddress, setBillingAddress] = useState("");
+  const [paymentMethod, setPaymentMethod] = useState({
+    cardNumber: "",
+    expirationDate: "",
+    cvv: "",
+  });
+  const navigate = useNavigate();
+  const cartList = useSelector(selectors.getProductCart);
+  const dispatch = useDispatch();
+  const ordersLen = useSelector(selectors.getOrders).length;
+
+  const checkout = () => {
+    const delay = 0.7 * Math.random() * 2 * 1000;
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve("ok");
+      }, delay);
+    });
+  };
+
+  const isDisabled = useMemo(() => {
+    const card = paymentMethod.cardNumber.replaceAll(" ", "");
+    return (
+      name.length === 0 ||
+      shippingAddress.length === 0 ||
+      card.length !== 16 ||
+      paymentMethod.cvv.length === 0 ||
+      paymentMethod.expirationDate.length === 0 ||
+      billingAddress.length === 0 ||
+      isLoading === true
+    );
+  }, [name, shippingAddress, paymentMethod, billingAddress]);
+
+  const changeNameHandler = useCallback((e) => {
+    setName(e.target.value);
+  }, []);
+  const changeCardHandler = useCallback((e) => {
+    setPaymentMethod((prev) => ({ ...prev, cardNumber: e.target.value }));
+  }, []);
+  const changeExDateHandler = useCallback((e) => {
+    setPaymentMethod((prev) => ({ ...prev, expirationDate: e.target.value }));
+  }, []);
+  const changeCVVHandler = useCallback((e) => {
+    setPaymentMethod((prev) => ({ ...prev, cvv: e.target.value }));
+  }, []);
+  const changeShippingHandler = useCallback((e) => {
+    setShippingAddress(e.target.value);
+  }, []);
+  const changeBillingHandler = useCallback((e) => {
+    setBillingAddress(e.target.value);
+  }, []);
+
+  const checkoutHandler = useCallback(async () => {
+    setIsLoading(true);
+    try {
+      await checkout();
+      const payment = { ...paymentMethod, cvv: parseInt(paymentMethod.cvv) };
+      const order: Order = {
+        id: ordersLen,
+        fullName: name,
+        shippingAddress,
+        billingAddress,
+        paymentMethod: payment,
+        products: cartList,
+      };
+      dispatch(actions.addOrder(order));
+    } catch (err) {
+    } finally {
+      setIsLoading(false);
+      navigate("/transaction-result");
+    }
+  }, [
+    dispatch,
+    name,
+    paymentMethod,
+    billingAddress,
+    shippingAddress,
+    cartList,
+  ]);
+
+  return {
+    cartList,
+    name,
+    shippingAddress,
+    billingAddress,
+    paymentMethod,
+    changeNameHandler,
+    changeCardHandler,
+    changeBillingHandler,
+    changeCVVHandler,
+    changeShippingHandler,
+    changeExDateHandler,
+    isDisabled,
+    checkoutHandler,
+  };
 };
--- src/spas/mini-store/scenes/CheckoutPage/index.tsx
+++ src/spas/mini-store/scenes/CheckoutPage/index.tsx
@@ -1,12 +1,191 @@
 import React, { memo } from "react";
 import { useCheckoutPage } from "./index.hooks";
+import {
+  Button,
+  Container,
+  Grid,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { CheckoutItem } from "@/components/CheckoutItem";
+import { styled } from "@mui/system";
+import { Link } from "react-router-dom";
 
 type CheckoutPageProps = {};
 
-export const CheckoutPage = memo(({}: CheckoutPageProps) => {
-  const {} = useCheckoutPage();
+const StyledButton = styled(Button)({
+  color: "#0f1a24",
+  background: "#d2dde2",
+  width: 300,
+  height: 50,
+  "&:hover": {
+    opacity: 0.9,
+    background: "#d2dde2",
+  },
+  "&:disabled": {
+    opacity: 0.2,
+    color: "#0f1a24",
+    background: "#d2dde2",
+  },
+});
 
-  return <div>Checkout Page</div>;
+const StyledTextField = styled(TextField)({
+  width: "60%",
+  color: "#d2dde2",
+});
+
+export const CheckoutPage = memo(({}: CheckoutPageProps) => {
+  const {
+    cartList,
+    shippingAddress,
+    billingAddress,
+    name,
+    paymentMethod,
+    changeShippingHandler,
+    changeCVVHandler,
+    changeExDateHandler,
+    changeBillingHandler,
+    changeCardHandler,
+    changeNameHandler,
+    isDisabled,
+    checkoutHandler,
+  } = useCheckoutPage();
+
+  return (
+    <Container
+      maxWidth={false}
+      sx={{ background: "#0f1a24", height: "100vh", color: "#D2DDE2", p: 4 }}
+    >
+      <Stack alignItems="center">
+        <Typography variant="h4" mb={2}>
+          Checkout Page
+        </Typography>
+        {cartList.map((prod) => (
+          <CheckoutItem key={prod.id} product={prod} />
+        ))}
+        <Typography fontWeight={600} my={2}>
+          Total amount:{" "}
+          {cartList
+            .map((el) => el.qty * el.price)
+            .reduce((acc, curr) => acc + curr, 0)}
+          €
+        </Typography>
+        <Grid container width="80%">
+          <Grid
+            item
+            xs={6}
+            display="flex"
+            direction="column"
+            alignItems="center"
+            rowGap={2}
+          >
+            <StyledTextField
+              label="Nome titolare carta"
+              value={name}
+              onChange={(e) => changeNameHandler(e)}
+              inputProps={{
+                style: {
+                  border: "1px solid #d2dde2",
+                  color: "#d2dde2",
+                  borderRadius: 5,
+                },
+              }}
+              InputLabelProps={{ sx: { color: "#d2dde2" } }}
+            />
+            <StyledTextField
+              label="Numero carta"
+              onChange={(e) => changeCardHandler(e)}
+              value={paymentMethod.cardNumber}
+              inputProps={{
+                style: {
+                  border: "1px solid #d2dde2",
+                  color: "#d2dde2",
+                  borderRadius: 5,
+                },
+              }}
+              InputLabelProps={{ sx: { color: "#d2dde2" } }}
+            />
+            <StyledTextField
+              label="Data di scadenza"
+              onChange={(e) => changeExDateHandler(e)}
+              value={paymentMethod.expirationDate}
+              inputProps={{
+                style: {
+                  border: "1px solid #d2dde2",
+                  color: "#d2dde2",
+                  borderRadius: 5,
+                },
+              }}
+              InputLabelProps={{ sx: { color: "#d2dde2" } }}
+            />
+            <StyledTextField
+              label="CVV"
+              onChange={(e) => changeCVVHandler(e)}
+              value={paymentMethod.cvv}
+              inputProps={{
+                style: {
+                  border: "1px solid #d2dde2",
+                  color: "#d2dde2",
+                  borderRadius: 5,
+                },
+              }}
+              InputLabelProps={{ sx: { color: "#d2dde2" } }}
+            />
+          </Grid>
+          <Grid
+            item
+            xs={6}
+            display="flex"
+            direction="column"
+            alignItems="center"
+            rowGap={2}
+          >
+            <StyledTextField
+              label="Indirizzo di spedizione"
+              onChange={(e) => changeShippingHandler(e)}
+              value={shippingAddress}
+              inputProps={{
+                style: {
+                  border: "1px solid #d2dde2",
+                  color: "#d2dde2",
+                  borderRadius: 5,
+                },
+              }}
+              InputLabelProps={{ sx: { color: "#d2dde2" } }}
+            />
+            <StyledTextField
+              label="Indirizzo di fatturazione"
+              onChange={(e) => changeBillingHandler(e)}
+              value={billingAddress}
+              inputProps={{
+                style: {
+                  border: "1px solid #d2dde2",
+                  color: "#d2dde2",
+                  borderRadius: 5,
+                },
+              }}
+              InputLabelProps={{ sx: { color: "#d2dde2" } }}
+            />
+            <Stack pt={3} rowGap={2} alignItems="center">
+              <StyledButton
+                onClick={checkoutHandler}
+                disabled={isDisabled}
+                variant="contained"
+              >
+                Conferma ordine
+              </StyledButton>
+              <Link to="/">
+                <Typography>
+                  <u>Torna alla Home</u>
+                </Typography>
+              </Link>
+            </Stack>
+          </Grid>
+        </Grid>
+      </Stack>
+    </Container>
+  );
 });
 
 CheckoutPage.displayName = "CheckoutPage";
